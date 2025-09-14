a18f45ab8f8171885a7e15de42ba0f476459cf60
Stella Sikhila <stellavinmaris@gmail.com>
2025-09-14 09:58:57 +0300

    fixed ui

--- src/app/features/dashboard/sections/left-section/left-section.component.ts
+++ src/app/features/dashboard/sections/left-section/left-section.component.ts
@@ -15,12 +15,12 @@ import { ServerCardsSectionComponent } from '../../../../shared/components/serve
 export class LeftSectionComponent {
   readonly descriptionData = {
     title: 'Description',
-    content: 'Lorem Ipsum Dolor Sit Amet Consectetur. Tempus a id adipiscing fames egestas tellus dis pretium tempus. Lorem ipsum dolor sit amet consectetur. Tempus a id adipiscing fames egestas tellus dis pretium tempus.'
+    content: 'Lorem ipsum dolor sit amet consectetur. Aenean sodales pellentesque gravida nibh et magna faucibus. Dui commodo ut metus amet egestas habitant viverra. Quisque fusce senectus facilisis non diam leo nulla sem pellentesque. Sit in vel sed cursus metus sit fringilla vestibulum.'
   };
 
   readonly extraData = {
     title: 'Extra',
-    content: 'Lorem ipsum dolor sit amet consectetur. Tempus a id adipiscing fames egestas tellus dis pretium tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit. Tempus a id adipiscing fames egestas tellus dis pretium tempus adipiscing fames.'
+    content: 'Lorem ipsum dolor sit amet consectetur. Tempus a id adipiscing fames egestas tellus dis pretium tempus. Justo nisl nisl lorem lectus id ornare. Rhoncus in egestas in amet porttitor pellentesque sit. Amet gravida integer velit felis. Eu consectetur interdum auctor sed aliquam. Eu pulvinar accumsan sed id. Duis a aliquam eu quisque commodo lectus. Lectus ipsum velit purus viverra vulputate viverra in nunc nulla. Euismod rhoncus mauris urna orci gravida sagittis netus. Amet mus in vel etiam. Interdum habitant congue massa in etiam sit. Commodo nibh viverra lobortis augue lorem quam lorem suspendisse.'
   };
 
   readonly keyValueItems: KeyValueItem[] = [
